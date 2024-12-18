e8ca0da5fec8fa65050787e8aac3238d6655d78d
MarkLumongsod14 <mark.adrian.lumongsod@gmail.com>
2024-12-18 09:57:07 +0800

    my_profile.php

--- modules/my_profile.php
+++ modules/my_profile.php
@@ -1,5 +1,5 @@
 
 <h1>MARK ADRIAN LUMONGSOD</h1>
     <p>IT student/p>
-    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+    <p>I am 20 years old, lived in Gumamela ext Carmen Cagayan de Oro City.</p>
  
