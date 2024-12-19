fa4919b7b85b0d867637324de05bbe006da7d9d4
NAMOC <kristinedais10@gmail.com>
2024-12-19 09:34:13 +0800

    NAMOC

---
+++ namoc.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NAMOC chan</title>
+</head>
+<body>
+    <h1>HI EMREWAN nis is NAMOC NROMERTH</h1>
+    <p>This my paragraph that will reach the heaven</p>
+    <table>
+        <th>NAME:</th>
+        <th>AGE:</th>
+    </table>
+    
+</body>
+</html>
