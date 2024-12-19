ca37b757bc8b37efc2184475de3182563db6d25c
unknown <irpo.gamutan.coc@phinmaed.com>
2024-12-19 09:30:48 +0800

    Gamutan

--- modules/my_profile.php
+++ modules/my_profile.php
@@ -1,5 +1,5 @@
 
-<h1>Jeremiah B Simo</h1>
+<h1>Irishdwin Gamutan</h1>
     <p>IT instructor</p>
     <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
  
