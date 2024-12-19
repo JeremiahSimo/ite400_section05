3ed7776d5f27a9a91f7717517f4ffa699539f9a7
Nacaya <nacaya.michael123@gmail.com>
2024-12-19 14:25:26 -0700

    my_profile.php

--- modules/my_profile.php
+++ modules/my_profile.php
@@ -1,5 +1,5 @@
 
-<h1>Jeremiah B Simo</h1>
-    <p>IT instructor</p>
-    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+<h1>Michael I. Nacaya</h1>
+    <p>IT Student</p>
+    <p>HAHAHAAHAHAHAHAHAHA</p>
  
